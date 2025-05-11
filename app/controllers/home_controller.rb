eba4592c3788e6af95c4ca250cecbeaa2d45b2d9
Lucy C <lucychen96@gmail.com>
2025-05-10 20:24:15 -0400

    Remove old commented code. Remove extra newlines in between code blocks.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,3 @@
-# app logic, routes, and controllers
-
 # Route to return new random color when button is clicked
 
 class HomeController < ApplicationController
@@ -59,35 +57,3 @@ class HomeController < ApplicationController
     redirect_to root_path(new_color: true), notice: "All colors reset. Start fresh!"
   end
 end
-
-
-# class HomeController < ApplicationController
-#     def index
-#       @hex_color = RandomColor.random_color
-#       #   @random_color = "#" + "%06x" % (rand * 0xffffff)
-#     end
-# end
-
-
-# class HomeController < ApplicationController
-#   def index
-#     if params[:new_color] == "true"
-#       @hex_color = RandomColor.random_color
-#       session[:current_color] = @hex_color
-#     else
-#       @hex_color = session[:current_color] || RandomColor.random_color
-#       session[:current_color] ||= @hex_color
-#     end
-#   end
-
-#   def vote
-#     color = session[:current_color]
-#     vote_type = params[:vote]
-
-#     # You can implement storing this vote in a database or log later
-#     # For now, just log it
-#     Rails.logger.info "Color #{color} voted as #{vote_type}"
-
-#     head :ok # Respond with no content
-#   end
-# end
