20a1b8b62350ccc1ba6b85ac682ae7fa5c2f4a65
Lucy C <lucychen96@gmail.com>
2025-05-10 22:50:44 -0400

    Set same_site attribute to 'lax' to prevent session issues caused by modern browser cookie restrictions.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,1 +1,4 @@
-Rails.application.config.session_store :cookie_store, key: '_color_ranker_session', expire_after: 1.day
+Rails.application.config.session_store :cookie_store, 
+  key: '_color_ranker_session', 
+  expire_after: 1.day,
+  same_site: :lax
