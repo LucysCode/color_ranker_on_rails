d8cb86c7f3361e10eb1efa0c75eb7442c4a89c44
Lucy C <lucychen96@gmail.com>
2025-05-10 20:07:46 -0400

    Fix bug where user can't continue voting for 'nice' colors after hitting maximum 'ugly' colors and vice versa. Add surprised pikachu face for when both 'nice' and 'ugly' colors are met. Add MAX_COLOR variable and instance variable. Edit @message for each condition.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,30 +3,37 @@
 # Route to return new random color when button is clicked
 
 class HomeController < ApplicationController
-  MAX_UGLY_COLORS = 5
-  MAX_NICE_COLORS = 5
+  MAX_COLORS = 3
+  MAX_UGLY_COLORS = MAX_COLORS
+  MAX_NICE_COLORS = MAX_COLORS
 
   def index
     @ugly_colors = ColorVote.where(is_ugly: true).order(created_at: :desc).limit(MAX_UGLY_COLORS).pluck(:hex_color)
-
     @nice_colors = ColorVote.where(is_nice: true).order(created_at: :desc).limit(MAX_NICE_COLORS).pluck(:hex_color)
-
+    @max_colors = MAX_COLORS
+  
     if params[:new_color] == "true"
-      if ColorVote.where(is_ugly: true).count >= MAX_UGLY_COLORS
-        @hex_color = nil
-        flash.now[:notice] = "You've already selected 5 ugly colors!"
-      elsif ColorVote.where(is_nice: true).count >= MAX_NICE_COLORS
-        @hex_color = nil
-        flash.now[:notice] = "You've already liked 5 colors!"
-      elsif ColorVote.count >= 16**6
-        @hex_color = nil
-        flash.now[:notice] = "All hex colors have been rated!"
+      if ColorVote.count >= 16**6
+        @hex_color = "#FFFFFF"
+        @message = "Woah you went through 16,777,216 colors? That's some serious dedication."
+      elsif
+        ColorVote.where(is_ugly: true).count >= MAX_UGLY_COLORS && ColorVote.where(is_nice: true).count >= MAX_NICE_COLORS
+        @image_url = "https://i.kym-cdn.com/entries/icons/facebook/000/027/475/Screen_Shot_2018-10-25_at_11.02.15_AM.jpg"
+        @show_pikachu = true
+        @message = "You selected too many ugly and nice colors! Let's reset :)"
       else
         begin
           new_color = RandomColor.random_color
         end while ColorVote.exists?(hex_color: new_color)
+  
         session[:current_color] = new_color
         @hex_color = new_color
+  
+        if ColorVote.where(is_ugly: true).count >= MAX_UGLY_COLORS
+          @message = "You selected too many ugly colors!"
+        elsif ColorVote.where(is_nice: true).count >= MAX_NICE_COLORS
+          @message = "You selected too many nice colors!"
+        end
       end
     else
       @hex_color = session[:current_color]
@@ -49,7 +56,7 @@ class HomeController < ApplicationController
   def reset
     ColorVote.delete_all
     session[:current_color] = nil
-    redirect_to root_path(new_color: true), notice: "All votes reset. Start fresh!"
+    redirect_to root_path(new_color: true), notice: "All colors reset. Start fresh!"
   end
 end
 
