299053f63b5d3b4bc1a1b4541bf8ebb5971b619a
Lucy C <lucychen96@gmail.com>
2025-05-19 22:43:41 -0400

    Add comments to understand code better.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,26 +1,38 @@
+# Defines a controller class called HomeController, that inherits from ApplicationController, which allows it to gain access to common behavior, like session handling, filters, etc. 
 class HomeController < ApplicationController
+  # Defines constants. Constants in Ruby are written in all caps and should not be changed. Stored once on the Ruby class level.
   MAX_COLORS = 3
   MAX_UGLY_COLORS = MAX_COLORS
   MAX_NICE_COLORS = MAX_COLORS
 
+  # Defines controller action. Usually tied to main/home page "/". The following is the method that runs when a user visit this route "/".
   def index
+    # Finds all votes for ugly=true in user's session, and orders them by their position field
+    # :position, :session_id, etc. are like headers in a spreadsheets. These headers change the structure of the backend database, so the command for migratiion must be used. 
+      # Example: rails generate migration AddPositionToColorVotes position:integer > 'enter' then rails db:migrate > 'enter'
+      # Add a position column of type integer to the color_votes table. Apply that change to your database.
+      # session[:session_id] means, "in this session, this is the session id"
     @ugly_colors = ColorVote.where(session_id: session[:session_id], is_ugly: true)
                          .order(:position)
 
     @nice_colors = ColorVote.where(session_id: session[:session_id], is_nice: true)
                          .order(:position)
-
+    # Assigns constant to instance variable. @max_colors can now be accessed in the view (index.html.erb), unlike MAX_COLORS, which is only available in the Ruby class.
     @max_colors = MAX_COLORS
 
     if params[:new_color] == "true"
+      puts "new color is TRUE"
+      # If all hex colors are done, stop generating colors. Send a message.
       if ColorVote.where(session_id: session[:session_id]).count >= 16**6
         @hex_color = "#FFFFFF"
         @message = "Woah you went through 16,777,216 colors? That's some serious dedication."
+      # If limit for ugly and nice colors are reached, stop generating colors. Send a message. Include a picture.
       elsif ColorVote.where(session_id: session[:session_id], is_ugly: true).count >= MAX_UGLY_COLORS &&
         ColorVote.where(session_id: session[:session_id], is_nice: true).count >= MAX_NICE_COLORS
         @image_url = "https://i.kym-cdn.com/entries/icons/facebook/000/027/475/Screen_Shot_2018-10-25_at_11.02.15_AM.jpg"
         @show_pikachu = true
         @message = "You ranked all the colors! Wanna reset? :)"
+      # Otherwise generate new colors.
       else
         begin
           new_color = RandomColor.random_color
@@ -28,19 +40,22 @@ class HomeController < ApplicationController
 
         session[:current_color] = new_color
         @hex_color = new_color
-
+        
+        # While generating new colors, if limit for ugly colors reached, send a message.
         if ColorVote.where(session_id: session[:session_id], is_ugly: true).count >= MAX_UGLY_COLORS
           @message = "You selected too many ugly colors!"
+        # While generating new colors, if limit for nice colors reached, send a message.
         elsif ColorVote.where(session_id: session[:session_id], is_nice: true).count >= MAX_NICE_COLORS
           @message = "You selected too many nice colors!"
         end
       end
     else
       @hex_color = session[:current_color]
+      puts "New color is false BLAH"
     end
   end
 
-  # Backend logic for when he list order is changed
+  # Backend logic for when the list order is changed
   def update_position
     ordered_ids = params[:ordered_ids]
     list_type = params[:list_type]
