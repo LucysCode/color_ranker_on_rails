3a1270d1f5779d58ec71e8a2900582eacdda3843
Lucy C <lucychen96@gmail.com>
2025-06-03 23:43:57 -0400

    Add user_id to color_votes and color_pair_votes.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_06_02_034352) do
+ActiveRecord::Schema[8.0].define(version: 2025_06_03_033551) do
   create_table "color_pair_votes", force: :cascade do |t|
     t.string "left_color"
     t.string "right_color"
@@ -21,6 +21,9 @@ ActiveRecord::Schema[8.0].define(version: 2025_06_02_034352) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "color_pair"
+    t.integer "user_id"
+    t.string "vote_type"
+    t.index ["user_id"], name: "index_color_pair_votes_on_user_id"
   end
 
   create_table "color_votes", force: :cascade do |t|
@@ -33,6 +36,19 @@ ActiveRecord::Schema[8.0].define(version: 2025_06_02_034352) do
     t.integer "position"
     t.string "left_color"
     t.string "right_color"
+    t.integer "user_id"
+    t.string "vote_type"
+    t.index ["user_id"], name: "index_color_votes_on_user_id"
+  end
+
+  create_table "pair_votes", force: :cascade do |t|
+    t.string "color1"
+    t.string "color2"
+    t.string "vote_type"
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_pair_votes_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -47,4 +63,18 @@ ActiveRecord::Schema[8.0].define(version: 2025_06_02_034352) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
+  create_table "votes", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.string "color", null: false
+    t.string "vote_type", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_votes_on_user_id"
+  end
+
+  add_foreign_key "color_pair_votes", "users"
+  add_foreign_key "color_votes", "users"
+  add_foreign_key "pair_votes", "users"
+  add_foreign_key "votes", "users"
 end
