4e0ecc2cce6d27abea67d9d556d419a65aa35e31
Lucy C <lucychen96@gmail.com>
2025-07-28 14:39:09 -0400

    Added example for 2nd PR in a PR/review stack where changes are based on PR1

--- config/application.rb
+++ config/application.rb
@@ -3,6 +3,7 @@ require_relative "boot"
 require "rails/all"
 
 # This is change in PR1
+# This is change in PR2
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
